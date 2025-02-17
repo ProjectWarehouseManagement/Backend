961beb03e77d996d24425dea4e5f544df1adf368
Juhasz Istvan Gyula <kaitoujap@gmail.com>
2025-02-17 18:00:25 +0100

    Add admin controller

--- src/admins/admins.controller.ts
+++ src/admins/admins.controller.ts
@@ -11,8 +11,8 @@ export class AdminsController {
   constructor(private readonly adminsService: AdminsService) {}
 
   @Post()
-  @ApiOperation({summary: 'Create a new Admin'})
-  @ApiResponse({ status: 201, description: 'The Admin has been succcesfully created.', type: Admin})
+  @ApiOperation({summary: 'Create a new Admin.'})
+  @ApiResponse({ status: 201, description: 'The Admin has been successfully created.', type: Admin})
   @ApiResponse({status: 400, description: 'Bad Request.'})
   @ApiBody({ type: CreateAdminDto })
   async create(@Body() createAdminDto: CreateAdminDto): Promise<admin> {
@@ -20,22 +20,37 @@ export class AdminsController {
   }
 
   @Get()
-  findAll() {
+  @ApiOperation({summary: 'Get all admin user.'})
+  @ApiResponse({ status: 200, description: 'The admin users have been successfully retrieved.', type: [Admin]})
+  async findAll(): Promise<admin[]> {
     return this.adminsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  @ApiOperation({summary: 'Find one admin user.'})
+  @ApiResponse({ status: 200, description: 'The admin user have been successfully retrieved.', type: Admin})
+  @ApiResponse({ status: 404, description: 'Admin not found.' })
+  @ApiBody({ type: CreateAdminDto })
+  async findOne(@Param('id') id: string): Promise<admin> {
     return this.adminsService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAdminDto: UpdateAdminDto) {
+  @ApiOperation({summary: 'Update an admin user.'})
+  @ApiResponse({ status: 200, description: 'The Admin has been successfully updated.', type: Admin})
+  @ApiResponse({status: 400, description: 'Bad Request.'})
+  @ApiResponse({ status: 404, description: 'Admin not found.' })
+  @ApiBody({ type: CreateAdminDto })
+  async update(@Param('id') id: string, @Body() updateAdminDto: UpdateAdminDto): Promise<admin> {
     return this.adminsService.update(+id, updateAdminDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  @ApiOperation({summary: 'Delete an admin user.'})
+  @ApiResponse({ status: 200, description: 'The Admin has been successfully deleted.', type: Admin})
+  @ApiResponse({ status: 404, description: 'Admin not found.' })
+  @ApiBody({ type: CreateAdminDto })
+  async remove(@Param('id') id: string): Promise<admin> {
     return this.adminsService.remove(+id);
   }
 }
