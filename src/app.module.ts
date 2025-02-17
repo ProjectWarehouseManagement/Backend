77d9f228e8ca0edc340510e600f965f135ce91e3
PatsFan12 <tamas.deme.andras12@gmail.com>
2025-02-17 15:26:45 +0100

    init admin endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminsModule } from './admins/admins.module';
 
 @Module({
-  imports: [],
+  imports: [AdminsModule],
   controllers: [AppController],
   providers: [AppService],
 })
