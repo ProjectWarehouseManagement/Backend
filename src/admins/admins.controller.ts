ee98cc1a1e74ddcacce98bcc0b88240d47f78432
Juhasz Istvan Gyula <kaitoujap@gmail.com>
2025-02-17 17:31:15 +0100

    Add admin service and controller

--- src/admins/admins.controller.ts
+++ src/admins/admins.controller.ts
@@ -2,13 +2,20 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { AdminsService } from './admins.service';
 import { CreateAdminDto } from './dto/create-admin.dto';
 import { UpdateAdminDto } from './dto/update-admin.dto';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { admin } from '@prisma/client';
+import { Admin } from './entities/admin.entity';
 
 @Controller('admins')
 export class AdminsController {
   constructor(private readonly adminsService: AdminsService) {}
 
   @Post()
-  create(@Body() createAdminDto: CreateAdminDto) {
+  @ApiOperation({summary: 'Create a new Admin'})
+  @ApiResponse({ status: 201, description: 'The Admin has been succcesfully created.', type: Admin})
+  @ApiResponse({status: 400, description: 'Bad Request.'})
+  @ApiBody({ type: CreateAdminDto })
+  async create(@Body() createAdminDto: CreateAdminDto): Promise<admin> {
     return this.adminsService.create(createAdminDto);
   }
 
