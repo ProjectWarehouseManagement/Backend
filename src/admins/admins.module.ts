23bfedd17e842879f8a96a2b2081370e42e9350a
Juhasz Istvan Gyula <kaitoujap@gmail.com>
2025-02-17 17:31:15 +0100

    Add admin service and controller

--- src/admins/admins.module.ts
+++ src/admins/admins.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AdminsService } from './admins.service';
 import { AdminsController } from './admins.controller';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
+  imports: [],
   controllers: [AdminsController],
-  providers: [AdminsService],
+  providers: [AdminsService, PrismaService],
 })
 export class AdminsModule {}
