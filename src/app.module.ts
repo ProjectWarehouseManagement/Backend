90244f68b2b045ea394e1c3e0356346e293e7a35
Juhasz Istvan Gyula <kaitoujap@gmail.com>
2025-02-18 16:31:28 +0100

    init user service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminsModule } from './admins/admins.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AdminsModule, AuthModule],
+  imports: [AdminsModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService]
 })
