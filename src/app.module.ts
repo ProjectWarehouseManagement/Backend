27a60e9fe0de5de64e12e2e002f95bddb9cbcc74
PatsFan12 <tamas.deme.andras12@gmail.com>
2025-02-18 15:29:45 +0100

    init product endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminsModule } from './admins/admins.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [AdminsModule],
+  imports: [AdminsModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService]
 })
