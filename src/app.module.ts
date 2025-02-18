14767dde048527ec3968eecb7796b7d482267e33
PatsFan12 <tamas.deme.andras12@gmail.com>
2025-02-18 14:44:59 +0100

    providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminsModule } from './admins/admins.module';
+import { ProvidersModule } from './providers/providers.module';
 
 @Module({
-  imports: [AdminsModule],
+  imports: [AdminsModule, ProvidersModule],
   controllers: [AppController],
   providers: [AppService]
 })
