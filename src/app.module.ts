ba62dc0317c1eacda78eeac9474e4ab05367e62d
Juhasz Istvan Gyula <kaitoujap@gmail.com>
2025-02-18 16:29:24 +0100

    init auth endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminsModule } from './admins/admins.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AdminsModule],
+  imports: [AdminsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService]
 })
